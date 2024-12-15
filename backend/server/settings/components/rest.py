f7a7747a46c4f22d4c4f5178a0890e3c3420faff
Patryk Beberok <patrol0990@gmail.com>
2024-12-15 17:43:55 +0100

    Updated default permission class to block get requests

--- backend/server/settings/components/rest.py
+++ backend/server/settings/components/rest.py
@@ -9,7 +9,7 @@ from server.settings.components.common import DATETIME_INPUT_FORMATS
 REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissions',
+        'server.utils.api.permissions.DjangoModelPermissionsWithGetPermissions',
     ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
